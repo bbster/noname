7019e8c48de41024ce816decd8415f4fef027333
bbster <bbster12@naver.com>
2022-07-15 22:32:36 +0900

    DB setting

--- scrapy_api/settings.py
+++ scrapy_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scrapy_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'noname_db',
+        'USER': 'noname_user',
+        'PASSWORD': '1r2r3r4r',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
