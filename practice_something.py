d489a6d0c170463e4a0dad785466a8089566aba9
bbster <bbster12@naver.com>
2022-08-05 23:11:41 +0900

    feat: book페이지 크롤링 및 데이터 출력

--- practice_something.py
+++ practice_something.py
@@ -2,7 +2,7 @@ from urllib.error import HTTPError
 from urllib.request import urlopen
 import ssl
 import re
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 # context = ssl._create_unverified_context()
 # html = urlopen('https://www.pythonscraping.com/pages/warandpeace.html', context=context)
@@ -27,26 +27,29 @@ from bs4 import BeautifulSoup
 
 # for child in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
 #     print(child)
-from urllib.parse import quote, unquote
-pages = set()
-
-def get_links(page_url):
-    page_url = quote(unquote(page_url))
-    global pages
-    context = ssl._create_unverified_context()
-    html = urlopen('https://ko.wikipedia.org{}'.format(page_url), context=context)
-    bs = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-
-    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
-        if 'href' in link.attrs:
-            if link.attrs['href'] not in pages:
-                new_page = link.attrs['href']
-                print(f"add_newpage: https://ko.wikipedia.org/{new_page} // {unquote(new_page)}")
-                pages.add(new_page)
-                get_links(new_page)
-
-get_links('')
+# from urllib.parse import quote, unquote
+# pages = set()
+#
+# def get_links(page_url):
+#     page_url = quote(unquote(page_url))
+#     global pages
+#     context = ssl._create_unverified_context()
+#     html = urlopen('https://ko.wikipedia.org{}'.format(page_url), context=context)
+#     bs = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
+#
+#     for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+#         if 'href' in link.attrs:
+#             if link.attrs['href'] not in pages:
+#                 new_page = link.attrs['href']
+#                 print(f"add_newpage: https://ko.wikipedia.org/{new_page} // {unquote(new_page)}")
+#                 pages.add(new_page)
+#                 get_links(new_page)
+#
+# get_links('')
 # for link in bs.find('div', {'id': 'bodyContent'}).find_all('a', href = re.compile('^(/wiki/)((?!:).)*$')):
 #
 #     if 'href' in link.attrs:
 #         print(f"link: {link.attrs['href']}")
+
+# t = eval("max([1, 2, 3, 4])")
+# print(t)
--- spider/books_crawler/books_crawler/spiders/books.py
+++ spider/books_crawler/books_crawler/spiders/books.py
@@ -1,23 +1,58 @@
-from scrapy import Selector, Request
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from selenium import webdriver
+from scrapy.http import Request
+from scrapy import Spider
+
+def product_info(response, value):
+    return response.xpath('//th[text()="'+ value +'"]/following-sibling::td/text()').extract_first()
 
 
-class BooksSpider(CrawlSpider):
+class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com']
 
-    def start_requests(self):
-        self.driver = webdriver.Chrome("C:\chromedriver")
-        self.driver.get("http://books.toscrape.com/")
+    def parse(self, response):
+        books = response.xpath('//h3/a/@href').extract()
 
-        sel = Selector(text=self.driver.page_source)
-        books = sel.xpath('//h3/a/@href').extract()
         for book in books:
-            url = 'http://books.toscrape.com/' + book
-            yield Request(url, callback=self.parse_book)
+            absolute_url = response.urljoin(book)
+            yield Request(absolute_url, callback=self.parse_book)
+
+        # next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
 
     def parse_book(self, response):
-        pass
+        title = response.css('h1::text').extract_first()
+        price = response.xpath('//*[@class="price_color"]/text()').extract_first()
 
+        image_url = response.xpath('//img/@src').extract_first()
+        image_url = image_url.replace('../..', 'http://books.toscrape.com/')
+
+        rating = response.xpath('//*[contains(@class, "star-rating")]/@class').extract_first()
+        rating = rating.replace('star-rating', '')
+
+        description = response.xpath('//*[@id="product_description"]/following-sibling::p/text()').extract_first()
+
+        upc = product_info(response, "UPC")
+        product_type = product_info(response, "Product Type")
+        price_excl = product_info(response, "Price (excl. tax)")
+        price_incl = product_info(response, "Price (incl. tax)")
+        tax = product_info(response, "Tax")
+        availability = product_info(response, "Availability")
+        number_of_reviews = product_info(response, "Number of reviews")
+
+        yield {
+            'title': title,
+            'price': price,
+            'image_url': image_url,
+            'rating': rating,
+            'description': description,
+            'upc': upc,
+            'product_type': product_type,
+            'price_excl': price_excl,
+            'price_incl': price_incl,
+            'tax': tax,
+            'availability': availability,
+            'number_of_reviews': number_of_reviews,
+        }
