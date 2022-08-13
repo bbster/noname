9e2856eeb0d5f82149c07628ba200d076fc4194c
bbster <bbster12@naver.com>
2022-08-13 16:29:47 +0900

    practice

--- spider/classcentral_spider/classcentral_spider/spiders/classcentral.py
+++ spider/classcentral_spider/classcentral_spider/spiders/classcentral.py
@@ -12,7 +12,7 @@ class ClasscentralSpider(Spider):
 
     def parse(self, response):
         if self.subject:
-            subject_url = response.xpath('//a[contains(@title, "Programming")]/@href').extract_first()
+            subject_url = response.xpath('//a[contains(@title, "'+self.subject +'")]/@href').extract_first()
             absolute_subject_url = response.urljoin(subject_url)
             yield Request(absolute_subject_url, callback=self.parse_subject)
         else:
@@ -23,4 +23,23 @@ class ClasscentralSpider(Spider):
                 yield Request(absolute_subject_url, callback=self.parse_subject)
 
     def parse_subject(self, response):
-        pass
+        subject_name = response.xpath('//h1/text()').extract_first()
+        cources = response.xpath('//li[@itemtype="http://schema.org/Event"]')
+
+        for i in range(len(cources)):
+
+            cource_name = cources[i].xpath('//*[@itemprop="name"]/text()').extract()[i]
+            # cource_name = cource.xpath('//*[@class="color-charcoal course-name"]/h2/text()').extract()
+            cource_url = cources[i].xpath('//*[@class="color-charcoal course-name"]/@href').extract()[i]
+            absolute_cource_url = response.urljoin(cource_url)
+
+            yield {
+                'subject_name': subject_name,
+                'cource_name': cource_name,
+                'absolute_cour_url': absolute_cource_url
+            }
+
+        next_page = cources[0].xpath('//link[@rel="next"]/@href').extract_first()
+        if next_page:
+            absolute_next_page = response.urljoin(next_page)
+            yield Request(absolute_next_page, callback=self.parse_subject)
